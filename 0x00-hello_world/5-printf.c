2716d57b67305aa6be85b3bf4d08ccf5cc8d2f81
zolakulani <shikwenizolakulani@gmail.com>
2023-07-13 14:54:38 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print a string using printf
+ * Return: Alway 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

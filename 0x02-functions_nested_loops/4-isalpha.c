452e5d3d0c1b3907e7f35505124ed90ed722d5c8
zolakulani <shikwenizolakulani@gmail.com>
2023-07-18 07:56:50 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,13 +3,14 @@
 /**
  * _isalpha - function that checks if a character
  * @c: the character to be checked
+ *
  * Return: 1 if c is a letter, 0 otherwise
  */
 
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }

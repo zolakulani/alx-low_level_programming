f64d0d1f5abff98585af91758708b006f18ef065
zolakulani <shikwenizolakulani@gmail.com>
2023-07-18 06:08:43 -0700

    void print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,15 @@
  */
 void print_alphabet_x10(void)
 {
-    char letter;
-    int newLine;
+char letter;
+int newLine;
 
-    for (newLine = 0; newLine < 10; newLine++)
-    {
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-            _putchar(letter);
-        }
-        _putchar('\n');
-    }
+for (newLine = 0; newLine < 10; newLine++)
+{
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+ 		_putchar(letter);
+	}
+	_putchar('\n');
+	}
 }

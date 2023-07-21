30a52968870b2d22e66742b24e947cafa8b9ef88
zolakulani <shikwenizolakulani@gmail.com>
2023-07-21 11:22:34 -0700

    ./4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _putchar - Writes a character to the standard output (stdout)
+ * @c: The character to be written
+ * Return: On success, 1. On error, -1.
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
+

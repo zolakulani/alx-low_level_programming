3a0888db01ea791ee14917df26f1a042da8905ba
zolakulani <shikwenizolakulani@gmail.com>
2023-07-25 16:19:32 -0700

    rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,14 +8,18 @@
 
 void rev_string(char *s)
 {
-	int len = strlen(s);
+	int len = 0;
 	char temp;
 
-	for (int i = 0, j = len - 1; i < j; i++, j--)
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (int i = 0; i < len / 2; i++)
 	{
 		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
 	}
 }
-

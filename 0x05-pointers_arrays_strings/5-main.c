d3814c9b2a67a3e07307a31167e1ba88a80e9084
zolakulani <shikwenizolakulani@gmail.com>
2023-07-25 13:07:07 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-main.c
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -1,29 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * rev_string - Reverses a string in-place.
- * @s: The string to be reversed.
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-void rev_string(char *s)
+int main(void)
 {
-    char *start = s;
-    char *end = s;
+    char s[10] = "My School";
 
-    // Move the end pointer to the end of the string
-    while (*end)
-        end++;
-
-    // Move the end pointer one step back to the last character
-    end--;
-
-    // Swap characters from start and end until they meet in the middle
-    while (start < end)
-    {
-        char temp = *start;
-        *start = *end;
-        *end = temp;
-
-        start++;
-        end--;
-    }
+    printf("%s\n", s);
+    rev_string(s);
+    printf("%s\n", s);
+    return (0);
 }

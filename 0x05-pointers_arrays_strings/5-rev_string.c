4a781efc8f58bd718a343133f7e1233a8acfe40b
zolakulani <shikwenizolakulani@gmail.com>
2023-07-25 12:29:55 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,12 +1,16 @@
+/* main.h (assumed to contain the function prototype for _putchar) */
+
+/* Function to print every other character of a string */
+void puts2(char *str);
+
+
+/* main.c */
+
 #include "main.h"
 
 /**
- * _putchar - custom putchar function (not shown here, assumed to be defined)
- */
-
-/**
- * puts2 - prints every other character of a string
- * @str: the input string
+ * puts2 - Prints every other character of a string
+ * @str: The input string
  */
 void puts2(char *str)
 {
@@ -14,12 +18,26 @@ void puts2(char *str)
 
     while (str[i] != '\0')
     {
-        if (i % 2 == 0) // Print every other character (even-indexed characters)
+        if (i % 2 == 0) /* Print every other character (even-indexed characters) */
         {
             _putchar(str[i]);
         }
         i++;
     }
 
-    _putchar('\n'); // Print a newline after printing the characters
+    _putchar('\n'); /* Print a newline after printing the characters */
+}
+
+/**
+ * main - Check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    char *str;
+
+    str = "0123456789";
+    puts2(str);
+    return (0);
 }

911143c7c7e5035d39e7e62003e77d19dafd869f
zolakulani <shikwenizolakulani@gmail.com>
2023-07-28 09:34:22 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,31 +1,39 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
- * @str: string to capitalize
- *
- * Return: pointer to the resulting string
+ * cap_string - capitalize all words of a string
+ * @str: string
+ * Return: `str`
  */
+
 char *cap_string(char *str)
 {
-	int i;
-	int is_word = 1;
+	int i, c;
+	int trigger;
+	char nots[] = ",;.!?(){}\n\t\" ";
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0, trigger = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (str[0] > 96 && str[0] < 123)
+			trigger = 1;
+		for (c = 0; nots[c] != '\0'; c++)
 		{
-			if (is_word)
+			if (nots[c] == str[i])
+				trigger = 1;
+		}
+
+		if (trigger)
+		{
+			if (str[i] > 96 && str[i] < 123)
 			{
 				str[i] -= 32;
+				trigger = 0;
 			}
-			is_word = 0;
-		}
-		else
-		{
-			is_word = 1;
+			else if (str[i] > 64 && str[i] < 91)
+				trigger = 0;
+			else if (str[i] > 47 && str[i] < 58)
+				trigger = 0;
 		}
 	}
-
 	return (str);
 }

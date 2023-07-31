1a4d925436d21f5cf37373932640c74fdffd688e
zolakulani <shikwenizolakulani@gmail.com>
2023-07-31 15:03:25 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *s = "hello, world";
+	char *f = "world";
+	char *t;
+
+	t = strstr(s, f);
+	printf("%s\n", t);
+
+	return (0);
+}
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - Locate a substring.
+ * @haystack: The string to search.
+ * @needle: The substring to find.
+ *
+ * Return: Pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *h, *n;
+
+	if (*needle == '\0')
+	return (haystack);
+
+	while (*haystack)
+	{
+		h = haystack;
+		n = needle;
+
+		while (*haystack && *n && *haystack == *n)
+		{
+			haystack++;
+			n++;
+		}
+
+		if (*n == '\0')
+			return (h);
+
+		haystack = h + 1;
+	}
+
+	return (NULL);
+}

1d3eebf2c43e669ca41f27cbbe6edd8bc19d0a81
zolakulani <shikwenizolakulani@gmail.com>
2023-08-02 07:57:25 -0700

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,11 +8,13 @@
  */
 int is_prime_number(int n)
 {
+	int i;
+
 	if (n <= 1)
 		return (0); /* Not a prime number */
 
 	/* Check for divisors from 2 to the square root of n */
-	for (int i = 2; i * i <= n; i++)
+	for (i = 2; i * i <= n; i++)
 	{
 		if (n % i == 0)
 			return (0); /* Not a prime number */

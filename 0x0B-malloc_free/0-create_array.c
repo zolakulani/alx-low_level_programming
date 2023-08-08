092ed7fa00d97393784117635b72202e23c670b5
zolakulani <shikwenizolakulani@gmail.com>
2023-08-08 15:35:24 -0700

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,25 +1,26 @@
 #include "main.h"
 
 /**
- * create_array-creates an array of chars
- * @size: size of arry
- * @C: charactor to store in array
- * Return: pointer to the array
- * /
- 
+ * *create_array - creates an array of chars,
+ * and initializes it with a specific char
+ * @size: size of the array to create
+ * @c: char to initialize the array c
+ *
+ * Return: pointer to the array (Success), NULL (Error)
+ */
 char *create_array(unsigned int size, char c)
 {
-	unsigned int;
+	unsigned int i;
 	char *arr;
-	
-	if (size ==0)
+
+	if (size == 0)
 		return (NULL);
 
-	arr = malloc(sizeof(char)*size);
+	arr = malloc(sizeof(char) * size);
 
-	if( arr == NULL)
+	if (arr == NULL)
 		return (NULL);
-	for (i = 0: i < size : i++)
+	for (i = 0; i < size; i++)
 		arr[i] = c;
-	return = arr
+	return (arr);
 }
--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -13,14 +13,14 @@ void simple_print_buffer(char *buffer, unsigned int size)
 {
 	unsigned int i;
 
- 	i= 0;
- 	while (i < size)
+	i = 0;
+	while (i < size)
 	{
 		if (i % 10)
 		{
 			printf(" ");
 		}
-		 if (!(i % 10) && i)
+		if (!(i % 10) && i)
 		{
 			printf("\n");
 		}

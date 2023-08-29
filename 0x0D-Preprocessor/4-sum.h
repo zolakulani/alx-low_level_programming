b1e3564356dc12d313d73f3f761b90af39848e29
zolakulani <shikwenizaolkulani@gmail.com>
2023-08-29 16:49:23 +0200

    done

--- 0x0D-Preprocessor/4-sum.h
+++
@@ -1,6 +0,0 @@
-#ifndef SUM_H_
-#define SUM_H_
-
-#define SUM(x, y) (x + y)
-
-#endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUNCTION_LIKE_MACRO_H_
-#define FUNCTION_LIKE_MACRO_H_
-
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-
-#endif

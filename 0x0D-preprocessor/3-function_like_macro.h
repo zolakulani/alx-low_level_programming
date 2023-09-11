bb658ba3c9d3893563bd7cb66b1de68a8fafa20b
zolakulani <shikwenizolakulani@gmail.com>
2023-09-11 05:03:20 -0700

    doen

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _FUNCTION_LIKE_MACRO_
-#define _FUNCTION_LIKE_MACRO_
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) |x|
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif

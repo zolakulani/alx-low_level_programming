8c3624657a108dd768766f01a84c9a8683fb5307
zolakulani <shikwenizolakulani@gmail.com>
2023-09-12 14:47:58 -0700

    struct_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,34 +13,34 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
-    char *new_name, *new_owner;
+	dog_t *new_dog;
+	char *new_name, *new_owner;
 
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
 
-    new_name = malloc(strlen(name) + 1);
-    if (new_name == NULL)
-    {
-        free(new_dog);
-        return (NULL);
-    }
+	new_name = malloc(strlen(name) + 1);
+	if (new_name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
 
-    new_owner = malloc(strlen(owner) + 1);
-    if (new_owner == NULL)
-    {
-        free(new_name);
-        free(new_dog);
-        return (NULL);
-    }
+	new_owner = malloc(strlen(owner) + 1);
+	if (new_owner == NULL)
+	{
+		free(new_name);
+		free(new_dog);
+		return (NULL);
+	}
 
-    strcpy(new_name, name);
-    strcpy(new_owner, owner);
+	strcpy(new_name, name);
+	strcpy(new_owner, owner);
 
-    new_dog->name = new_name;
-    new_dog->age = age;
-    new_dog->owner = new_owner;
+	new_dog->name = new_name;
+	new_dog->age = age;
+	new_dog->owner = new_owner;
 
-    return (new_dog);
+	return (new_dog);
 }

29238ec65a14950edf2d6496535218e92daf83c2
zolakulani <shikwenizolakulani@gmail.com>
2023-09-12 11:33:10 -0700

    struct_dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,7 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_;
+}dog_t;
 	/* Additional function prototypes */
 	int _putchar(char c);
 	void init_dog(struct dog *d, char *name, float age, char *owner);

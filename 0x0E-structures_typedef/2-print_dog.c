ea4acf1503418beeb96875ee92e546dabb2820be
zolakulani <shikwenizolakulani@gmail.com>
2023-09-12 11:43:18 -0700

    struct_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,10 +7,10 @@
   */
 void print_dog(struct dog *d)
 {
-	if (d)
+	if (d != NULL)
 	{
 		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
-		printf("Age: %f\n", (d->age) ? d-> age : 0);
+		printf("Age: %f\n", (d->age) ? d->age : 0);
 		printf("owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
 }

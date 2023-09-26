d9da381c30383547c146e51dc4eb709c213cc032
zolakulani <shikwenizolakulani@mail.com>
2023-09-26 02:49:28 -0700

    2_elements

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,21 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked listint_t list
- * @h: pointer to the head of the list
- * Return: the number of elements in the list
+ *  listint_len - is to determine the count of nodes in a linked list
+ * @h: represents the head of the list_
+ *
+ * Return: the total number of nodes in the list_
  */
+
 size_t listint_len(const listint_t *h)
 {
-    size_t count = 0;
+        const listint_t *temp_k = h;
+        size_t count_k = 0;
 
-    while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
-
-    return (count);
+        while (temp_k != NULL)
+        {
+                count_k++;
+                temp_k = temp_k->next;
+        }
+        return (count_k);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,6 +19,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+size_t listint_len(const listint_t *h);
 size_t print_listint(const listint_t *h);
 listint_t *find_listint_loop(listint_t *head);
 size_t free_listint_safe(listint_t **h);

e532aea52427435744c9890ffd3eb1f986e0e341
zolakulani <shikwenizolakulani@mail.com>
2023-09-26 14:43:26 -0700

    done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,17 +4,17 @@
  * print_listint - is used to display all the elements in a linked list
  * @h: represents the head of the list
  *
- * Return: the # of nodes
+ * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *temp_k = h;
 	size_t count_k = 0;
 
-	while (count_k != NULL)
+	while (temp_k != NULL)
 	{
 		printf("%d\n", temp_k->n);
-		count += 1;
+		count_k += 1;
 		temp_k = temp_k->next;
 	}
 	return (count_k);

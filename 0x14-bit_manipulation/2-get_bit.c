ad19f351e7bb6d7dee9756518750635b30ddfa26
zolakulani <shikwenizolakulani@mail.com>
2023-10-01 07:58:29 -0700

    crackme3

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,25 +1,22 @@
 #include "main.h"
 
 /**
- * get_bit - returns the value of a bit at a given
+ * get_bit - returns value of a bit at a given
  * index.
- * @n: unsigned long int input.
- * @index: index of the bit.
+ * @n: unsigned long int feed.
+ * @index: index possition of the bit.
  *
- * Return: value of the bit.
+ * Return: value of the bit at index.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
-	if (n == 0 && index < 64)
-		return (0);
-
-	for (i = 0; i <= 63; n >>= 1, i++)
+	for (i = 0; i <= 32;i++)
 	{
 		if (index == i)
 		{
-			return (n & 1);
+			return (n &= (n - 1));
 		}
 	}
 
